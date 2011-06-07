e7fe7501914eb4bcd8e98d57df4ae039393d1407
Joe Wheaton <joseph.wheaton@gmail.com>
2011-06-07 08:52:00 -0700

    Added in active_record.js and fixes for it.

--- Resources/app.js
+++ Resources/app.js
@@ -1,7 +1,8 @@
 Ti.UI.setBackgroundColor('#ffffff');
 
 var shl = {};
-Ti.include('db.js')
+Ti.include('active_record.js');
+Ti.include('db.js');
 Ti.include('ui.js');
 
 Ti.API.info('Welcome to Outreach for '+Ti.Platform.osname);
