787d6ccac4f1e3199e61a5021044f9a7bace9fc8
Charles Meyer <robomaster@gmail.com>
2011-06-07 12:49:13 -0700

    first try

--- Resources/db.js
+++ Resources/db.js
@@ -1,4 +1,45 @@
 (function() {
-  shl.db = {};
-  
+  //ActiveRecord.logging = true;
+  //ActiveSupport.log = Ti.API.debug;
+
+  /* Set the Adapter to Titanium and connect to the default 'app.sqlite' database */
+  ActiveRecord.connect(ActiveRecord.Adapters.Titanium, "Outreach");
+
+  //******************** Prospect *****************************
+  shl.Prospect = ActiveRecord.create('prospects', {
+    last : '',
+    firstMale : '',
+    firstFemale : '',
+    street : '',
+    city : '',
+    state : '',
+    zip : '',
+    country : '',
+    phoneHome : '',
+    phoneMoble : '',
+    email : '',
+    firstContactDate : 0,
+    firstContactPoint : '',
+    previouslySaved : 0,
+    previouslyBaptized : 0,
+    sundaySchool : 0,
+    status : '',
+    nextStep : '',
+    lastContact : 0,
+    created : 0,
+    modified : 0,
+    uuid : ''
+  });
+
+  //************************* Tests ******************************************
+  var testProspect = shl.Prospect.create({
+    last: "Jones",
+    firstMale: "John",
+    email : "test@example.com"
+  });
+  Ti.API.info("***************" + testProspect.toJSON());
+  testProspect.save();
+  var foundProspect = shl.Prospect.find(testProspect.id);
+  Ti.API.info(foundProspect.toJSON());
+
 })();
