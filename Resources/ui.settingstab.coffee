818bf0a54f5014d98bd3b1e0b94249a58fd2dd79
Joe Wheaton <joseph.wheaton@gmail.com>
2011-09-02 10:50:01 -0700

    Settings page using the new SettingRow class.

--- Resources/ui.settingstab.coffee
+++ Resources/ui.settingstab.coffee
@@ -16,10 +16,43 @@ class SettingsTab
     })
     data = []
     
+    defaultSection = Ti.UI.createTableViewSection({
+      headerTitle: 'Default Settings'
+    })
+    defaultCityRow = new SettingRow({},{
+      name: 'defaultCity',
+      control: 'text',
+      value: Ti.App.Properties.getString('defaultCity', ' '),
+      title: 'City'
+    })
+    defaultStateRow = new SettingRow({},{
+      name: 'defaultState',
+      control: 'text',
+      value: Ti.App.Properties.getString('defaultState', ' '),
+      title: 'State'
+    })
+    defaultZipRow = new SettingRow({},{
+      name: 'defaultZip',
+      control: 'text',
+      value: Ti.App.Properties.getString('defaultZip', ' '),
+      title: 'Zip'
+    })
+    defaultCountryRow = new SettingRow({},{
+      name: 'defaultCountry',
+      control: 'text',
+      value: Ti.App.Properties.getString('defaultCountry', ' '),
+      title: 'Country'
+    })
+    defaultSection.add(defaultCityRow)
+    defaultSection.add(defaultStateRow)
+    defaultSection.add(defaultZipRow)
+    defaultSection.add(defaultCountryRow)
     
+    data.push(defaultSection)
     
     tableView = Ti.UI.createTableView({
-      data: data
+      data: data,
+      style: Titanium.UI.iPhone.TableViewStyle.GROUPED
     })
     settingsWin.add(tableView)
     
--- Resources/ui.settingstab.js
+++ Resources/ui.settingstab.js
@@ -11,14 +11,47 @@
       });
     }
     SettingsTab.prototype.createSettingsWindow = function() {
-      var data, self, settingsWin, tableView;
+      var data, defaultCityRow, defaultCountryRow, defaultSection, defaultStateRow, defaultZipRow, self, settingsWin, tableView;
       self = this;
       settingsWin = Ti.UI.createWindow({
         title: 'Settings'
       });
       data = [];
+      defaultSection = Ti.UI.createTableViewSection({
+        headerTitle: 'Default Settings'
+      });
+      defaultCityRow = new SettingRow({}, {
+        name: 'defaultCity',
+        control: 'text',
+        value: Ti.App.Properties.getString('defaultCity', ' '),
+        title: 'City'
+      });
+      defaultStateRow = new SettingRow({}, {
+        name: 'defaultState',
+        control: 'text',
+        value: Ti.App.Properties.getString('defaultState', ' '),
+        title: 'State'
+      });
+      defaultZipRow = new SettingRow({}, {
+        name: 'defaultZip',
+        control: 'text',
+        value: Ti.App.Properties.getString('defaultZip', ' '),
+        title: 'Zip'
+      });
+      defaultCountryRow = new SettingRow({}, {
+        name: 'defaultCountry',
+        control: 'text',
+        value: Ti.App.Properties.getString('defaultCountry', ' '),
+        title: 'Country'
+      });
+      defaultSection.add(defaultCityRow);
+      defaultSection.add(defaultStateRow);
+      defaultSection.add(defaultZipRow);
+      defaultSection.add(defaultCountryRow);
+      data.push(defaultSection);
       tableView = Ti.UI.createTableView({
-        data: data
+        data: data,
+        style: Titanium.UI.iPhone.TableViewStyle.GROUPED
       });
       settingsWin.add(tableView);
       return settingsWin;
