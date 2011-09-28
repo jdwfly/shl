7c87da5f4f3f1381d408a3f6344a55ffcab11942
Joe Wheaton <joseph.wheaton@gmail.com>
2011-09-27 17:14:02 -0700

    Major juryrig to fix last second android issues.

--- Resources/ui.settingstab.coffee
+++ Resources/ui.settingstab.coffee
@@ -1,4 +1,119 @@
-SettingRow = require('lib/utility').SettingRow
+#Ti.include('lib/utility.js')
+#SettingRow = require('lib/utility').SettingRow
+# This is a crime to all humanity
+class SettingRow
+  constructor: (args, settings) ->
+    self = this
+    instance = Ti.UI.createTableViewRow(args)
+    instance.settingName = settings.name
+    
+    # Just in case we need to debug
+    debug = if settings.debug? then settings.debug else false
+    
+    # add both labels: title and value
+    settingTitleLabel = Ti.UI.createLabel({
+      text: settings.title,
+      left: 5,
+      width: 'auto',
+      height: 'auto'
+    })
+    instance.add(settingTitleLabel)
+    @settingValueLabel = Ti.UI.createLabel({
+      text: settings.value,
+      right: if Ti.Platform.osname is 'iphone' then 5 else 30,
+      width: 150,
+      height: 'auto',
+      textAlign: 'right'
+    })
+    if settings.control isnt 'boolean'
+      instance.add(@settingValueLabel)
+    # Add the type of control
+    switch settings.control
+      when 'select', 'text'
+        instance.hasChild = true
+        
+        instance.addEventListener('click', (e) ->
+          # create a new window with one textfield with focus
+          optionsWin = Ti.UI.createWindow({
+            title: settings.title,
+            backgroundColor: if Ti.Platform.osname is 'iphone' then 'stripped' else '#181818'
+          })
+          if settings.control is 'select'
+            # add the list options
+            value = Ti.App.Properties.getString(settings.name)
+            data = []
+            for option in settings.options
+              row = Ti.UI.createTableViewRow({
+                title: option.name,
+                hasCheck: false
+              })
+              if option.name is value
+                row.hasCheck = true
+              data.push(row)
+            optionsTableView = Ti.UI.createTableView({
+              data: data,
+              style: Titanium.UI.iPhone.TableViewStyle.GROUPED
+            })
+            optionsTableView.addEventListener('click', (f) ->
+              if debug
+                Ti.API.info(f)
+              for row, i in optionsTableView.data[0].rows
+                if i is f.index
+                  optionsTableView.data[0].rows[i].hasCheck = true
+                  Ti.App.Properties.setString(settings.name, optionsTableView.data[0].rows[i].title)
+                  self.settingValueLabel.text = optionsTableView.data[0].rows[i].title
+                else
+                  optionsTableView.data[0].rows[i].hasCheck = false
+            )
+            optionsWin.add(optionsTableView)
+          else
+            # add the textfield option with keyboard focus
+            textField = Ti.UI.createTextField({
+              width: 300,
+              height: 40,
+              left: 10,
+              top: 10,
+              value: Ti.App.Properties.getString(settings.name),
+              keyboardType:Titanium.UI.KEYBOARD_DEFAULT,
+              returnKeyType:Titanium.UI.RETURNKEY_DONE,
+              borderStyle:Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
+            })
+            # Function saves property
+            saveProperty = (f) ->
+              if debug then Ti.API.info(settings.name + ' = ' + f)
+              Ti.App.Properties.setString(settings.name, f)
+              self.settingValueLabel.text = f
+              optionsWin.close()
+            textField.addEventListener('return', (f) ->
+              saveProperty(@value)
+            )
+            optionsWin.add(textField)
+            optionsWin.addEventListener('open', (f) ->
+              textField.focus()
+            )
+            Ti.API.info('Ti.Platform.osname = ' + Ti.Platform.osname)
+            if Ti.Platform.osname is 'android'
+              optionsWin.addEventListener('android:back', (f) ->
+                saveProperty(textField.value)
+              )
+          # this line doesn't make it very modular but I can't figure any other way to do this
+          shl.ui.tabs.activeTab.open(optionsWin)
+        )
+      # creates an on/off switch
+      when 'boolean'
+        value = Ti.App.Properties.getBool(settings.name)
+        settingSwitch = Ti.UI.createSwitch({
+          value: if value? then value else false,
+          right: 5
+        })
+        settingSwitch.addEventListener('change', (e) ->
+          if debug then Ti.API.info(settings.name + ' = ' + e.value)
+          Ti.App.Properties.setBool(settings.name, e.value)
+        )
+        instance.add(settingSwitch)
+    
+    return instance
+
 # Contains all the UI functions associated with the Settings Tab
 class SettingsTab
   constructor : () ->
--- Resources/ui.settingstab.js
+++ Resources/ui.settingstab.js
@@ -1,6 +1,126 @@
 (function() {
   var SettingRow, SettingsTab;
-  SettingRow = require('lib/utility').SettingRow;
+  SettingRow = (function() {
+    function SettingRow(args, settings) {
+      var debug, instance, self, settingSwitch, settingTitleLabel, value;
+      self = this;
+      instance = Ti.UI.createTableViewRow(args);
+      instance.settingName = settings.name;
+      debug = settings.debug != null ? settings.debug : false;
+      settingTitleLabel = Ti.UI.createLabel({
+        text: settings.title,
+        left: 5,
+        width: 'auto',
+        height: 'auto'
+      });
+      instance.add(settingTitleLabel);
+      this.settingValueLabel = Ti.UI.createLabel({
+        text: settings.value,
+        right: Ti.Platform.osname === 'iphone' ? 5 : 30,
+        width: 150,
+        height: 'auto',
+        textAlign: 'right'
+      });
+      if (settings.control !== 'boolean') {
+        instance.add(this.settingValueLabel);
+      }
+      switch (settings.control) {
+        case 'select':
+        case 'text':
+          instance.hasChild = true;
+          instance.addEventListener('click', function(e) {
+            var data, option, optionsTableView, optionsWin, row, saveProperty, textField, value, _i, _len, _ref;
+            optionsWin = Ti.UI.createWindow({
+              title: settings.title,
+              backgroundColor: Ti.Platform.osname === 'iphone' ? 'stripped' : '#181818'
+            });
+            if (settings.control === 'select') {
+              value = Ti.App.Properties.getString(settings.name);
+              data = [];
+              _ref = settings.options;
+              for (_i = 0, _len = _ref.length; _i < _len; _i++) {
+                option = _ref[_i];
+                row = Ti.UI.createTableViewRow({
+                  title: option.name,
+                  hasCheck: false
+                });
+                if (option.name === value) {
+                  row.hasCheck = true;
+                }
+                data.push(row);
+              }
+              optionsTableView = Ti.UI.createTableView({
+                data: data,
+                style: Titanium.UI.iPhone.TableViewStyle.GROUPED
+              });
+              optionsTableView.addEventListener('click', function(f) {
+                var i, row, _len2, _ref2, _results;
+                if (debug) {
+                  Ti.API.info(f);
+                }
+                _ref2 = optionsTableView.data[0].rows;
+                _results = [];
+                for (i = 0, _len2 = _ref2.length; i < _len2; i++) {
+                  row = _ref2[i];
+                  _results.push(i === f.index ? (optionsTableView.data[0].rows[i].hasCheck = true, Ti.App.Properties.setString(settings.name, optionsTableView.data[0].rows[i].title), self.settingValueLabel.text = optionsTableView.data[0].rows[i].title) : optionsTableView.data[0].rows[i].hasCheck = false);
+                }
+                return _results;
+              });
+              optionsWin.add(optionsTableView);
+            } else {
+              textField = Ti.UI.createTextField({
+                width: 300,
+                height: 40,
+                left: 10,
+                top: 10,
+                value: Ti.App.Properties.getString(settings.name),
+                keyboardType: Titanium.UI.KEYBOARD_DEFAULT,
+                returnKeyType: Titanium.UI.RETURNKEY_DONE,
+                borderStyle: Titanium.UI.INPUT_BORDERSTYLE_ROUNDED
+              });
+              saveProperty = function(f) {
+                if (debug) {
+                  Ti.API.info(settings.name + ' = ' + f);
+                }
+                Ti.App.Properties.setString(settings.name, f);
+                self.settingValueLabel.text = f;
+                return optionsWin.close();
+              };
+              textField.addEventListener('return', function(f) {
+                return saveProperty(this.value);
+              });
+              optionsWin.add(textField);
+              optionsWin.addEventListener('open', function(f) {
+                return textField.focus();
+              });
+              Ti.API.info('Ti.Platform.osname = ' + Ti.Platform.osname);
+              if (Ti.Platform.osname === 'android') {
+                optionsWin.addEventListener('android:back', function(f) {
+                  return saveProperty(textField.value);
+                });
+              }
+            }
+            return shl.ui.tabs.activeTab.open(optionsWin);
+          });
+          break;
+        case 'boolean':
+          value = Ti.App.Properties.getBool(settings.name);
+          settingSwitch = Ti.UI.createSwitch({
+            value: value != null ? value : false,
+            right: 5
+          });
+          settingSwitch.addEventListener('change', function(e) {
+            if (debug) {
+              Ti.API.info(settings.name + ' = ' + e.value);
+            }
+            return Ti.App.Properties.setBool(settings.name, e.value);
+          });
+          instance.add(settingSwitch);
+      }
+      return instance;
+    }
+    return SettingRow;
+  })();
   SettingsTab = (function() {
     function SettingsTab() {
       this.win = this.createSettingsWindow();
